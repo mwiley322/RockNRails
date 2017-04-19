7f51c6c348ce9b649469977f5e45db1d0492728a
Megan Wiley <mwiley322@gmail.com>
2017-04-19 00:48:21 -0700

    Add create method for album controller and associated route

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,4 +8,13 @@ class AlbumsController < ApplicationController
   def new
     @album = Album.new
   end
+  def create
+    Album.create(album_params)
+    redirect_to("/albums")
+  end
+  
+  private
+  def album_params
+    params.require(:album).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/albums" => "albums#index", as: "albums"
   get "/albums/new" => "albums#new", as: "new_album"
   get "/albums/:id" => "albums#show", as: "album"
+  post "/albums" => "albums#create"
 end
